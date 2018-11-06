33ba8df653c48cc54dd5a04202abf616b6a0c66b
Aaron Jackson <aarontjdev@gmail.com>
2018-11-05 18:36:50 -0800

    3.5_update

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* jshint node:true */
 'use strict';
 
@@ -10,4 +11,10 @@ module.exports = {
     'nested-interactive': false,
     'bare-strings':true
   }
+=======
+'use strict';
+
+module.exports = {
+  extends: 'recommended'
+>>>>>>> d6c7d526... message
 };
